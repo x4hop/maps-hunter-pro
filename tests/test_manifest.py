bb87067e70d43d11c21a24be5a9db52111a8c1b8
x4hop <anas98gha@gmail.com>
2026-09-08 21:46:20 +0200

    Fix signup action and remove Start permission prompt

--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -5,8 +5,8 @@ root=Path(__file__).parents[1]/'extension'
 m=json.loads((root/'manifest.json').read_text())
 refs=[m['background']['service_worker'],m['action']['default_popup'],m['side_panel']['default_path'],*m['icons'].values()]
 for ref in refs: assert (root/ref).is_file(),ref
-assert 'https://*/*' not in m.get('host_permissions',[])
-assert 'https://*/*' in m.get('optional_host_permissions',[])
+assert 'https://*/*' in m.get('host_permissions',[])
+assert not m.get('optional_host_permissions',[])
 for file in root.rglob('*'):
     if file.suffix in {'.js','.html'}:
         text=file.read_text(errors='ignore')
